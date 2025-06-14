e4316b353947ed662bcc2992ed5c7abe17cda979
theanarchist123 <2023.nikhil.kadam@ves.ac.in>
2025-06-14 23:36:46 +0530

    Routing initial setup done..

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AllUsers = () => {
+  return (
+    <div>
+      UsersTable
+    </div>
+  )
+}
+
+export default AllUsers
---
+++ app/routes/admin/dashboard.tsx
@@ -0,0 +1,8 @@
+
+const dashboard = () => {
+  return (
+    <div>Dashboard Page</div>
+  )
+}
+
+export default dashboard
